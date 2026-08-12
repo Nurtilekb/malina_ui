20b0d0011ac7bcffb81cef89aa8fe5a93d211633
Nurtilek <abdlhtv06@gmail.com>
2026-08-12 18:36:26 +0300

    refactor: improve layout and styling of OrderActionButton for better UI consistency

--- lib/widgets/order_action_button.dart
+++ lib/widgets/order_action_button.dart
@@ -22,47 +22,58 @@ class OrderActionButton extends StatelessWidget {
   final Color? textcolor;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 15),
-      width: wodth1 ?? double.infinity,
-      height: height1 ?? 56,
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: textcolor?.withValues(alpha: 0.2) ?? Colors.white,
-          width: 0.4,
-        ),
-        color: color1 ?? const Color(0xFFF72055),
-        borderRadius: radous1 == null
-            ? BorderRadius.circular(14)
-            : BorderRadius.circular(radous1!),
-      ),
-      child: Center(
-        child: Row(
-          mainAxisAlignment: price == null
-              ? MainAxisAlignment.center
-              : MainAxisAlignment.start,
-          children: [
-            Text(
-              text,
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w500,
-                color: textcolor ?? Colors.white,
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: const Color(0xFFF72055),
+        child: InkWell(
+          borderRadius: BorderRadius.circular(12),
+          onTap: () {},
+
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            width: wodth1 ?? double.infinity,
+            height: height1 ?? 56,
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: textcolor?.withValues(alpha: 0.2) ?? Colors.white,
+                width: 0.4,
+              ),
+              color: color1 ?? const Color.fromARGB(33, 247, 32, 86),
+              borderRadius: radous1 == null
+                  ? BorderRadius.circular(14)
+                  : BorderRadius.circular(radous1!),
+            ),
+            child: Center(
+              child: Row(
+                mainAxisAlignment: price == null
+                    ? MainAxisAlignment.center
+                    : MainAxisAlignment.start,
+                children: [
+                  Text(
+                    text,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: textcolor ?? Colors.white,
+                    ),
+                  ),
+
+                  if (price != null) ...[
+                    const Spacer(),
+                    Text(
+                      price!,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ],
               ),
             ),
-
-            if (price != null) ...[
-              const Spacer(),
-              Text(
-                price!,
-                style: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w700,
-                  color: Colors.white,
-                ),
-              ),
-            ],
-          ],
+          ),
         ),
       ),
     );
